64ddd6b92b8fb3f7e2f6a5a3788396c90c0b7368
Md. Rezaul Karim <96794203+devRezaulKarim@users.noreply.github.com>
2024-05-18 09:32:23 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -27,9 +27,6 @@ server.use(jsonServer.rewriter({
     //to access by any property (applicable for only clothings)
     '/clothings/:key/:value': '/clothings?:key=:value',
 
-
-'/clothings/:key/:value/:key/:value': '/clothings?:key=:value:key=:value',
-
 }))
 server.use(router)
 server.listen(3000, () => {
